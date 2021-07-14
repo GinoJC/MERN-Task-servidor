c73b51032ef87febc0ff6329f0b5049a36b31017
GinoJC <gino.jc@hotmail.com>
2021-07-14 19:35:03 -0300

    -

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const app = express();
 // conectar a la DB
 conectarDB();
 
+// habilitar cors
 app.use(cors());
 
 // habilitar express.json
 app.use(express.json({ extended: true }));
 
 // puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 // importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -23,4 +24,4 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
-app.listen(port, '0.0.0.0', () => console.log(`Listening in the port ${port}`));
+app.listen(port, () => console.log(`Listening in the port ${port}`));
